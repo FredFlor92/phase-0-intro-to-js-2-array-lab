af096168ad0296391d5abdf5bcc2543c1d58f065
Freddy Florial <freddyflorial@gmail.com>
2022-08-02 20:20:52 -0400

    commit tree a184e743653bf8c1f3514ad828c5e5c66cb13aab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+function appendCat(name){
+    const moreCats = [...cats, name];
+    return moreCats
+}
+function prependCat(name){
+    const moreCats = [name, ...cats];
+    return moreCats
+}
+function removeLastCat(){
+    const catRemovedArray = [...cats.slice(0, -1)];
+    return catRemovedArray;
+}
+function removeFirstCat(){
+    const catRemovedArray = [...cats.slice(1)];
+    return catRemovedArray;
+}
